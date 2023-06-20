44a11837a9ca8b83206b906e654c15db69b2bf00
Chris Spivey <theori373@gmail.com>
2023-06-19 18:18:55 -0700

    finsihed product

--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,24 @@
 const notesRouter = require("express").Router()
 const { readFromFile, readAndAppend, writeToFile} = require("../utilities/utils")
+const { v4: uuidv4 } = require('uuid');
+
 
 
 notesRouter.get('/', (req, res) => {
     console.info(`${req.method} request for notes`);
-    readFromFile('../db/db.json').then((data) => res.json(JSON.parse(data)))
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
 })
 
+notesRouter.post('/', (req, res) => {
+    console.info(`${req.method} request to add note`)
+    const {title, text} = req.body
+    if (req.body) {const newNote = {
+        title,
+        text,
+        id: uuidv4()
+    }
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note successfully added`)
+}})
+
+module.exports = notesRouter
--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"))
 app.use("/api", api)
 
-app.get("/", (req, res) => res.sendFile(path.join(__dirname, "./public/index.hmtl")))
-app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "./public/notes.hmtl")))
-app.get("*", (req, res) => res.sendFile(path.join(__dirname, "./public/index.hmtl")))
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/public/index.html")))
+app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "/public/notes.html")))
+app.get("*", (req, res) => res.sendFile(path.join(__dirname, "/public/index.html")))
 
 app.listen(PORT, () =>  console.log(`App listening at http://localhost:${PORT}`))
